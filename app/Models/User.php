e260060a227f15ad1ab59f8cecb23709797ca7a3
xnguyenbaduyx <xnguyenbaduyx@gmail.com>
2025-03-12 11:17:54 +0700

    Lưu các thay đổi ở User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,27 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $primaryKey = 'User_Id'; 
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'User_Role',
+        'Email',
+        'Password',
+        'Last_Name',
+        'First_Name',
+        'Address',
+        'Phonenumber',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function role()
+    {
+        return $this->belongsTo(UserRole::class, 'User_Role', 'Role_Id');
+    }
 }
